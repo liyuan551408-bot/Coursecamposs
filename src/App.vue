d0dccd56189d121397ca85b50425b6455a0d5e9d
BeaRuby <863639635@qq.com>
2026-07-27 17:16:53 +1200

    week3:Project skeleton initialization

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">首页</router-link> |
+      <router-link to="/login">登录</router-link> |
+      <router-link to="/courses">课程列表</router-link> |
+      <router-link to="/dashboard">仪表盘</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
